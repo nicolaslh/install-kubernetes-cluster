68a7ce6fa2bec8c4d87a7c4b100ac2109f2efbd7
nicolaslh <815219233@qq.com>
2022-10-28 14:26:51 +0800

    Update environment.sh
    change ip for local env

--- manifests/environment.sh
+++ manifests/environment.sh
@@ -4,22 +4,22 @@
 export ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 
 # 集群各机器 IP 数组
-export NODE_IPS=(172.27.138.251 172.27.137.229 172.27.138.239)
+export NODE_IPS=(192.168.0.14 192.168.0.15 192.168.0.16)
 
 # 集群各 IP 对应的主机名数组
-export NODE_NAMES=(zhangjun-k8s-01 zhangjun-k8s-02 zhangjun-k8s-03)
+export NODE_NAMES=(nic-k8s-01 nic-k8s-02 nic-k8s-03)
 
 # etcd 集群服务地址列表
-export ETCD_ENDPOINTS="https://172.27.138.251:2379,https://172.27.137.229:2379,https://172.27.138.239:2379"
+export ETCD_ENDPOINTS="https://192.168.0.14:2379,https://192.168.0.15:2379,https://192.168.0.16:2379"
 
 # etcd 集群间通信的 IP 和端口
-export ETCD_NODES="zhangjun-k8s-01=https://172.27.138.251:2380,zhangjun-k8s-02=https://172.27.137.229:2380,zhangjun-k8s-03=https://172.27.138.239:2380"
+export ETCD_NODES="nic-k8s-01=https://192.168.0.14:2380,nic-k8s-02=https://192.168.0.15:2380,nic-k8s-03=https://192.168.0.16:2380"
 
 # kube-apiserver 的反向代理(kube-nginx)地址端口
 export KUBE_APISERVER="https://127.0.0.1:8443"
 
 # 节点间互联网络接口名称
-export IFACE="eth0"
+export IFACE="ens192"
 
 # etcd 数据目录
 export ETCD_DATA_DIR="/data/k8s/etcd/data"
